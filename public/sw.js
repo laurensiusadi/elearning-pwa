8cab60dd33d784f86694941f8b4499b8cc9b65eb
Laurensius Adi <laurensiusadi@gmail.com>
2017-04-12 12:42:07 +0700

    Service worker done

--- public/sw.js
+++ public/sw.js
@@ -11,11 +11,11 @@ var filesToCache = [
     '/offline',
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function(event) {
     console.log('[ServiceWorker] Install');
     self.skipWaiting();
     console.log('[ServiceWorker] Skip waiting');
-    e.waitUntil(
+    event.waitUntil(
         caches.open(cacheName).then(function(cache) {
             console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
@@ -23,9 +23,9 @@ self.addEventListener('install', function(e) {
     );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function(event) {
   console.log('[ServiceWorker] Activate');
-  e.waitUntil(
+  event.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== cacheName) {
@@ -40,42 +40,17 @@ self.addEventListener('activate', function(e) {
   );
 });
 
-self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url)
-    e.respondWith(
-        caches.open(cacheName).then(function(cache) {
-            return cache.match(e.request).then(function(response) {
-                var fetchPromise = fetch(e.request).then(function(networkResponse) {
-                    cache.put(e.request, networkResponse.clone());
-                    console.log('[ServiceWorker] Network Fetch', e.request.url)
-                    return networkResponse;
-                })
-                return response || fetchPromise;
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).then(function(response) {
+            var networkResponse = response.clone();
+            caches.open(cacheName).then(function(cache) {
+                cache.put(event.request, networkResponse);
             })
+            return response;
+        }).catch(function() {
+            console.log('[ServiceWorker] Cache Fallback', event.request.url);
+            return caches.match(event.request);
         })
     );
 });
-
-// self.addEventListener('fetch', function(e) {
-//     console.log('[ServiceWorker] Fetch', e.request.url)
-//     e.respondWith(
-//         fetch(e.request).catch(function() {
-//             console.log('[ServiceWorker] Return cache', e.request.url);
-//             return caches.match(e.request);
-//         })
-//     );
-// });
-
-// self.addEventListener('fetch', function(e) {
-//     console.log('[ServiceWorker] Fetch', e.request.url)
-//     e.respondWith(
-//         // console.log('[ServiceWorker] Return cache', e.request.url)
-//         caches.match(e.request).then(function(response) {
-//             // console.log('[ServiceWorker] Network Fallback', e.request.url);
-//             return response || fetch(e.request);
-//         }).catch(function() {
-//             console.log('[ServiceWorker] Cache Fallback', e.request.url)
-//             return caches.match(e.request);
-//         })
-//     );
-// });
